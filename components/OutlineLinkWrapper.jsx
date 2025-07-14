54289ca71bcc8106a44c65ecf5f53f01e6042b5c
NegiAbhijeet <abhijeetnegi.ab@gmail.com>
2025-07-14 17:23:53 +0530

    button ui

--- components/OutlineLinkWrapper.jsx
+++ components/OutlineLinkWrapper.jsx
@@ -2,7 +2,7 @@ import { Button } from "./ui/button";
 
 const OutlineLinkWrapper = ({ children, className = "", link = "", icon = true, ...props }) => {
     const baseClasses =
-        "relative inline-flex items-center px-12 py-3 overflow-hidden text-base font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50 text-white bg-transparent border border-white hover:opacity-90 transition";
+        "relative inline-flex items-center px-12 py-3 overflow-hidden text-base font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-black group hover:bg-gray-50 text-white bg-transparent border border-white hover:opacity-90 transition";
 
     return (
         <a
@@ -11,7 +11,7 @@ const OutlineLinkWrapper = ({ children, className = "", link = "", icon = true, 
         >
             <Button className={`${baseClasses} ${className}`}
                 {...props}>
-                <span className="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                <span className="absolute left-0 block w-full h-0 transition-all bg-white opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
 
                 {icon && (
                     <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
