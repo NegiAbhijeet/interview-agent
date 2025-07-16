cea0ed4e3e859dcc6e21b1e3711bd9b28e7ed303
NegiAbhijeet <abhijeetnegi.ab@gmail.com>
2025-07-16 14:13:43 +0530

    lauching sentence

--- components/LaunchToast.jsx
+++ components/LaunchToast.jsx
@@ -4,7 +4,7 @@ export default function TopBanner() {
   return (
     <div className="fixed top-0 w-full bg-black text-white text-center py-2 z-50 px-4">
       <p className="text-sm font-medium">
-        🚀 Don't Get Left Behind! The future of interviews is almost here. Get ready—you won't want to miss this!
+        🚀 Launching Soon: Don't Get Left Behind! The future of interviews is almost here. Get ready you won't want to miss this!
       </p>
     </div>
   );
