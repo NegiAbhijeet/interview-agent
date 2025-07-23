1cbd72be13f0752859c95461bab7b8a3057ec981
NegiAbhijeet <abhijeetnegi.ab@gmail.com>
2025-07-23 22:21:31 +0530

    line height

--- components/headingComponentWrapper.jsx
+++ components/headingComponentWrapper.jsx
@@ -1,11 +1,12 @@
-import HeadingWrapper from "@/components/headingWrapper"
+import HeadingWrapper from "@/components/headingWrapper";
+
 const HeadingComponentWrapper = ({ first, second, third, icon }) => {
     return (
         <div className="flex flex-col items-center justify-center space-y-4 text-center mb-8">
             <HeadingWrapper icon={icon} text={first} />
 
             <h2
-                className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-gradient"
+                className="text-3xl font-bold sm:text-4xl md:text-5xl text-gradient leading-tight pb-1 overflow-visible"
                 data-aos="fade-up"
                 data-aos-delay="200"
                 data-aos-duration="600"
@@ -14,7 +15,7 @@ const HeadingComponentWrapper = ({ first, second, third, icon }) => {
             </h2>
 
             <p
-                className="max-w-[900px] mx-auto text-gray-900/80 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+                className="max-w-[900px] mx-auto text-gray-900/80 text-xl leading-relaxed"
                 data-aos="fade-up"
                 data-aos-delay="200"
                 data-aos-duration="600"
@@ -22,7 +23,7 @@ const HeadingComponentWrapper = ({ first, second, third, icon }) => {
                 {third}
             </p>
         </div>
-    )
-}
+    );
+};
 
-export default HeadingComponentWrapper
+export default HeadingComponentWrapper;
